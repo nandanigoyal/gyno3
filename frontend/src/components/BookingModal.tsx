c9b95503e4efac42b5bef1e9bad3fc44055b7c87
nandani-goyal <152094228+nandani-goyal@users.noreply.github.com>
2026-07-29 03:57:04 +0530

    added url

--- frontend/src/components/BookingModal.tsx
+++ frontend/src/components/BookingModal.tsx
@@ -23,6 +23,7 @@ declare global {
     webkitSpeechRecognition: any;
   }
 }
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 const BookingModal = ({ isOpen, onClose, doctorName, bookingType }: BookingModalProps) => {
   const [selectedDate, setSelectedDate] = useState("");
@@ -167,7 +168,7 @@ const BookingModal = ({ isOpen, onClose, doctorName, bookingType }: BookingModal
         original_symptoms: symptoms
       };
 
-      await axios.post("http://127.0.0.1:8000/api/appointments", payload);
+      await axios.post("API_BASE_URL/api/appointments", payload);
 
       toast({
         title: "Booking Confirmed! ✅",
--- frontend/src/components/NearbyDoctors.tsx
+++ frontend/src/components/NearbyDoctors.tsx
@@ -20,7 +20,7 @@ const customIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 const NearbyDoctors = () => {
   const [location, setLocation] = useState<{ lat: number; lng: number } | null>(() => {
